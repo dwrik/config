54b96d19e87e7a58f1e5997bbc286601f2ce4ed1
Wrik Das <wrikdas22@gmail.com>
2020-12-10 11:05:05 +0530

    added git bash configs

---
+++ git/git-prompt.sh
@@ -0,0 +1,56 @@
+if test -f /etc/profile.d/git-sdk.sh
+then
+	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
+else
+	TITLEPREFIX=$MSYSTEM
+fi
+
+if test -f ~/.config/git/git-prompt.sh
+then
+	. ~/.config/git/git-prompt.sh
+else
+	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+	PS1="$PS1"'\[\033[94m\]'               # change to light blue
+	PS1="$PS1"'['                          # open braces
+	PS1="$PS1"'\[\033[34m\]'               # change to blue
+	PS1="$PS1"'\u'                         # user
+	PS1="$PS1"'\[\033[97m\]'               # change to white
+	PS1="$PS1"'@'                          # @
+	PS1="$PS1"'\[\033[91m\]'               # change to light red
+	PS1="$PS1"'\h '                        # show hostname
+	PS1="$PS1"'\[\033[96m\]'               # change to light cyan
+	PS1="$PS1"'\w'                         # current working directory
+	PS1="$PS1"'\[\033[94m\]'               # change to light blue
+	PS1="$PS1"']'                          # closing braces
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+			PS1="$PS1"'`__git_ps1`'   # bash function
+		fi
+	fi
+	PS1="$PS1"'\n'                  # newline
+	PS1="$PS1"'\[\033[96m\]'        # change to light cyan
+	PS1="$PS1"'$ '                  # prompt: always $
+	PS1="$PS1"'\[\033[97m\]'        # change to white
+fi
+
+MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+
+# Evaluate all user-specific Bash completion scripts (if any)
+if test -z "$WINELOADERNOEXEC"
+then
+	for c in "$HOME"/bash_completion.d/*.bash
+	do
+		# Handle absence of any scripts (or the folder) gracefully
+		test ! -f "$c" ||
+		. "$c"
+	done
+fi
